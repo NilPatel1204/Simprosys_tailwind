0c2d32908a3b6ee4e10cc0b54cccf33584a5141d
NilPatel1204 <nil@sanskartechnolab.com>
2024-12-30 19:03:40 +0530

    Formatted

---
+++ index.js
@@ -0,0 +1,60 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  freeMode: true,
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".slide-next",
+    prevEl: ".slide-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1, // Small screens
+    },
+    768: {
+      // Medium screens (md)
+      slidesPerView: 2,
+      spaceBetween: 24, // Optional spacing
+    },
+    1280: {
+      // Large screens
+      slidesPerView: 3,
+      spaceBetween: 24, // Optional spacing
+    },
+  },
+});
+
+document.querySelectorAll(".platform-btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    // Hide all content
+    document.querySelectorAll(".platform-content").forEach((content) => {
+      content.style.display = "none";
+    });
+
+    // Show the content for the selected platform
+    const platform = button.getAttribute("data-platform");
+    document.querySelector(
+      `.platform-content[data-platform="${platform}"]`
+    ).style.display = "grid";
+  });
+});
+
+// For Navbar
+
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (
+//     document.body.scrollTop > 300 ||
+//     document.documentElement.scrollTop > 300
+//   ) {
+//     document.getElementById("header").style.position = "fixed";
+//     document.getElementById("header").style.top = "0px";
+//     document.getElementById("header").style.animationName = "nav-animation";
+//   } else {
+//     document.getElementById("header").style.position = "initial";
+//     document.getElementById("header").style.animationName = "fad-1";
+//   }
+// }
